4f63e0f4d8b195016c902c8213c0cfb35e680f41
evg1kr <113228938+evg1kr@users.noreply.github.com>
2022-12-16 23:56:59 +0300

    Add files via upload

---
+++ laba3/thread.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <windows.h>
+struct threadArgs {
+    int* arr;
+    int n;
+    int num;
+    HANDLE actions[2];
+    threadArgs(int* _arr, int _n, int _num) : num(_num), n(_n) {
+        arr = new int[n];
+        for (int i = 0; i < n; i = -~i) {
+            arr[i] = _arr[i];
+        }
+        actions[0] = CreateEvent(NULL, FALSE, FALSE, NULL);
+        actions[1] = CreateEvent(NULL, FALSE, FALSE, NULL);
+    }
+};
